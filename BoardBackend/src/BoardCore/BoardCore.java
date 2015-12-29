26c8a46bb4ae16f0e85b283490519fc6b5069b51
Tobias <muellertobias789@gmail.com>
2015-12-29 23:14:41 +0100

    Services sind nun im Nameservice unter dem Namen der Tafel-ID zu finden.

--- BoardBackend/src/BoardCore/BoardCore.java
+++ BoardBackend/src/BoardCore/BoardCore.java
@@ -10,11 +10,15 @@ import BasicServices.*;
 
 import BoardModules.BasicServices.*;
 import BoardModules.AdvancedServices.*;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.omg.CORBA.ORB;
 import org.omg.CORBA.ORBPackage.InvalidName;
 import org.omg.CosNaming.*;
 import org.omg.CosNaming.NamingContextPackage.AlreadyBound;
 import org.omg.CosNaming.NamingContextPackage.CannotProceed;
+import org.omg.CosNaming.NamingContextPackage.NotEmpty;
 import org.omg.CosNaming.NamingContextPackage.NotFound;
 import org.omg.PortableServer.POA;
 import org.omg.PortableServer.POAHelper;
@@ -32,7 +36,6 @@ import org.omg.PortableServer.POAHelper;
 public class BoardCore {
     
     private ORB _orb;
-    private POA _rootPOA;
     private String _boardIdentifer;
     private BoardServiceImpl _boardService;
     private AdministrationServiceImpl _adminService;
@@ -42,49 +45,87 @@ public class BoardCore {
     
     public BoardCore(String[] args) {
         try {
-            this._boardIdentifer = "Test-Tafel";
+            this._boardIdentifer = "Test-Table1";
             
             // initialisierung ORB
             //String[] orbInitial = {"-ORBInitialPort ",  orbInitialPort, " -ORBInitialHost ", orbInitialHost };
-            this._orb = ORB.init(args, null);
+            Properties props = new Properties();
+            props.put("org.omg.CORBA.ORBInitialPort", "1050");
+            props.put("org.omg.CORBA.ORBInitialHost", "localhost");
             
-            _rootPOA = POAHelper.narrow(_orb.resolve_initial_references("RootPOA"));
-            _rootPOA.the_POAManager().activate();
+            this._orb = ORB.init(args, props);
+            
+            POA rootPOA = POAHelper.narrow(_orb.resolve_initial_references("RootPOA"));
+            rootPOA.the_POAManager().activate();
+  
+            
+            org.omg.CORBA.Object objRef = _orb.resolve_initial_references("NameService");
+            NamingContextExt initContext = NamingContextExtHelper.narrow(objRef);
             
             this._boardService = new BoardServiceImpl();
             this._adminService = new AdministrationServiceImpl();
             this._viewService = new ViewServiceImpl();
             this._virtualBoardService = new VirtualBoardServiceImpl();
             
-            org.omg.CORBA.Object boardServiceRef = _rootPOA.servant_to_reference(_boardService);
-            org.omg.CORBA.Object adminServiceRef = _rootPOA.servant_to_reference(_adminService);
-            org.omg.CORBA.Object viewServiceRef = _rootPOA.servant_to_reference(_viewService);
-            org.omg.CORBA.Object virtualBoardServiceRef = _rootPOA.servant_to_reference(_virtualBoardService);
+            org.omg.CORBA.Object boardServiceRef = rootPOA.servant_to_reference(_boardService);
+            org.omg.CORBA.Object adminServiceRef = rootPOA.servant_to_reference(_adminService);
+            org.omg.CORBA.Object viewServiceRef = rootPOA.servant_to_reference(_viewService);
+            org.omg.CORBA.Object virtualBoardServiceRef = rootPOA.servant_to_reference(_virtualBoardService);
             
             BoardService boardService = BoardServiceHelper.narrow(boardServiceRef);
             AdministrationService adminService = AdministrationServiceHelper.narrow(adminServiceRef);
             ViewService viewService = ViewServiceHelper.narrow(viewServiceRef);
             VirtualBoardService virtualBoardService = VirtualBoardServiceHelper.narrow(virtualBoardServiceRef);
             
-            org.omg.CORBA.Object objRef = _orb.resolve_initial_references("NameService");
-            NamingContextExt ncRef = NamingContextExtHelper.narrow(objRef);
+            NameComponent boardNC = new NameComponent(_boardIdentifer, "");
+            NameComponent boardServiceNC = new NameComponent("BoardService", "");
+            NameComponent adminServiceNC = new NameComponent("AdminService", "");
+            NameComponent viewServiceNC = new NameComponent("ViewService", "");
+            NameComponent virtualBoardServiceNC = new NameComponent("VirtualBoardService", "");
+
             
-            NameComponent home = new NameComponent(_boardIdentifer, "");
+            NameComponent path1[] = { boardNC, boardServiceNC };
+            NameComponent path2[] = { boardNC, adminServiceNC };
+            NameComponent path3[] = { boardNC, viewServiceNC, };
+            NameComponent path4[] = { boardNC, virtualBoardServiceNC };
             
             
             
-            bindServiceToNameService(home, "BoardService", boardService, ncRef);
-            bindServiceToNameService(home, "AdministrationService", adminService, ncRef);
-            bindServiceToNameService(home, "ViewService", viewService, ncRef);
-            bindServiceToNameService(home, "VirtualBoardService", virtualBoardService, ncRef);
+            registerObjWithNameService(initContext, path1, boardServiceRef);
+            registerObjWithNameService(initContext, path2, adminServiceRef);
+            registerObjWithNameService(initContext, path3, viewServiceRef);
+            registerObjWithNameService(initContext, path4, virtualBoardServiceRef);
             
-            while (true) {
-                _orb.run();
-            }
+            
+            
             
         } catch (Exception ex) {
-            System.out.println(ex.getLocalizedMessage());
+//            try {
+                
+                ex.printStackTrace();
+//                org.omg.CORBA.Object objRef = _orb.resolve_initial_references("NameService");
+//                NamingContextExt ncRef = NamingContextExtHelper.narrow(objRef);
+//                ncRef.unbind(ncRef.to_name(_boardIdentifer));
+//                int i = 1;
+//                BindingListHolder blh = new BindingListHolder();
+//                BindingIteratorHolder bih = new BindingIteratorHolder();
+//                ncRef.list(i, blh, bih);
+//            } catch (InvalidName ex1) {
+//                Logger.getLogger(BoardCore.class.getName()).log(Level.SEVERE, null, ex1);
+//            } catch (NotFound ex1) {
+//                Logger.getLogger(BoardCore.class.getName()).log(Level.SEVERE, null, ex1);
+//            } catch (CannotProceed ex1) {
+//                Logger.getLogger(BoardCore.class.getName()).log(Level.SEVERE, null, ex1);
+//            } catch (org.omg.CosNaming.NamingContextPackage.InvalidName ex1) {
+//                Logger.getLogger(BoardCore.class.getName()).log(Level.SEVERE, null, ex1);
+//            }
         } 
+    }
+    
+    public void run() {
+        while (true) {
+            _orb.run();
+        }
     }
     
     /**
@@ -98,11 +139,20 @@ public class BoardCore {
      * @throws CannotProceed
      * @throws InvalidName 
      */
-    private void bindServiceToNameService(NameComponent home, String serviceName, org.omg.CORBA.Object serviceRef, NamingContextExt namingContextRef) throws NotFound, CannotProceed, InvalidName, org.omg.CosNaming.NamingContextPackage.InvalidName, AlreadyBound {
+    private void registerObjWithNameService(NamingContext root, NameComponent[] serverName, org.omg.CORBA.Object obj) throws NotFound, AlreadyBound, CannotProceed, org.omg.CosNaming.NamingContextPackage.InvalidName {
+        NamingContext currentContext = root;
         
-        NameComponent path[] = {home, new NameComponent(serviceName, "")};
-        NamingContext cxt = namingContextRef.new_context();
-        cxt.bind_new_context(path);
+        NameComponent[] singleElement = new NameComponent[1];
+        for (int i = 0; i < serverName.length - 1; i++) {
+            try {
+                singleElement[0] = serverName[i];
+                currentContext = NamingContextHelper.narrow(currentContext.resolve(singleElement));
+            } catch (NotFound ex) {
+                currentContext = currentContext.bind_new_context(singleElement);
+            } 
+        }
+        singleElement[0] = serverName[serverName.length - 1];
+        currentContext.rebind(singleElement, obj);
     }
     
     /*
--- BoardBackend/src/BoardCore/BoardCoreApplication.java
+++ BoardBackend/src/BoardCore/BoardCoreApplication.java
@@ -24,5 +24,6 @@ public class BoardCoreApplication {
         */
          
          core = new BoardCore(args);
+         core.run();
     }
 }
