9314ce1a6e14a5297ea85a843d217abda9954242
Patrick <Patrick@10.9.42.208>
2016-01-07 10:49:29 +0100

    ForwardMessage erweitert

--- BoardBackend/src/BasicServices/AdministrationServiceImpl.java
+++ BoardBackend/src/BasicServices/AdministrationServiceImpl.java
@@ -12,10 +12,17 @@ import BoardModules.DestinationUnreachable;
 import BoardModules.Message;
 import BoardModules.User;
 import java.util.ArrayList;
+import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import org.omg.CORBA.ORB;
+import org.omg.CosNaming.NamingContextExt;
+import org.omg.CosNaming.NamingContextExtHelper;
 import org.omg.CosNaming.NamingContextPackage.CannotProceed;
 import org.omg.CosNaming.NamingContextPackage.InvalidName;
+import org.omg.CosNaming.* ;
+import org.omg.CosNaming.NamingContextPackage.*;
+import BoardModules.UnknownUser;
 
 /**
  *
@@ -49,25 +56,43 @@ public class AdministrationServiceImpl extends AdministrationServicePOA {
 
     @Override
     public void forwardMessageToBoards(String[] boards, Message message) throws DestinationUnreachable {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-        
-/*      boards.length in der 2.Schleife ist soll die Anzahl der aktiven Namen
-        im NameService sein
-        equals(boards), boards soll hier ein aktiver Name im NameService sein
-*/
-//        BoardServiceImpl bsi = new BoardServiceImpl();
-//        User user = new User("admin");
-//        for (int i = 0; i < boards.length; i++) {
-//            for (int j = i + 1; j <= boards.length; j++) {
-//                if (boards[i].equals(boards)) {
-//                    try {
-//                        bsi.sendMessage(user, message, boards[i]);
-//                    } catch (UnknownUser ex) {
-//                        System.out.println("User existiert nicht!");
-//                    }
-//                }
-//            }
-//        }
+	try {
+	    //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+	    ORB _orb;
+	    Properties props = new Properties();
+	    
+	    props.put("org.omg.CORBA.ORBInitialPort", "1050");
+	    props.put("org.omg.CORBA.ORBInitialHost", "localhost");
+	    
+	    _orb = ORB.init(new String[0], props);
+	    org.omg.CORBA.Object objRef = _orb.resolve_initial_references("NameService");
+	    NamingContextExt ncRef = NamingContextExtHelper.narrow(objRef);
+	    
+	    int batchSize = 100;
+	    BindingListHolder bList = new BindingListHolder();
+	    BindingIteratorHolder bIterator = new BindingIteratorHolder();
+	    
+	    ncRef.list(batchSize, bList, bIterator);
+	    /*      boards.length in der 2.Schleife ist soll die Anzahl der aktiven Namen
+	    im NameService sein
+	    equals(boards), boards soll hier ein aktiver Name im NameService sein
+	    */
+	BoardServiceImpl bsi = new BoardServiceImpl();
+        User user = new User("admin");
+        for (int i = 0; i < boards.length; i++) {
+            for (int j = 0; j < bList.value.length; j++) {
+                if (boards[i].equals(bList.value)) {
+                    try {
+                        bsi.sendMessage(user, message, boards[i]);
+                    } catch (UnknownUser ex) {
+                        System.out.println("User existiert nicht!");
+                    }
+                }
+            }
+        }
+	} catch (org.omg.CORBA.ORBPackage.InvalidName ex) {
+	    Logger.getLogger(AdministrationServiceImpl.class.getName()).log(Level.SEVERE, null, ex);
+	}
     }
 
     @Override
