3e45387c9d8fbfea85fdd999d0d32e7061c69152
user <user@HP>
2016-01-07 13:10:42 +0100

    User Speichern

--- BoardDataStorage/src/storage/StoredUsers.java
+++ BoardDataStorage/src/storage/StoredUsers.java
@@ -1,67 +1,129 @@
 package storage;
 
-/**
- * Wegen des Aufbaus der MessageParser Klasse, liest diese Klasse
- * den ersten Token aus der erstellten .txt raus, welcher
- * der Username sein soll, und gibt ihn aus.
- * Alleine wie man hier schon mit Objekten hantieren muss
- * um etwas einfaches rauszubekommen, gibt mir aber auch das Gefühl dass
- * die Idee mit der .txt Datei vielleicht nicht das optimale ist.
- * 
- * Höchstwahrscheinlich bin ich aber auch auf nem ganz falschen Pfad,
- * da ich mit mit Java, regulären Ausrücken, Substrings, etc eigentlich
- * auch nich wirklich auskenne
- * 
- * Am sinnvollsten wäre es, die Usernamen direkt bei der
- * Eingabe aus der GUI zu ziehen (Wozu man aber eine art DB bräuchte, 
- * wobei dann aber ein extra Service laufen muss)
- * Gilt auch für die gesendeten Nachrichten
- * 
- * 
- */
-
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Scanner;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  *
  * @author Matthäus Piechowiak
  */
 public class StoredUsers {
-  
-    private String readFile() throws IOException
+
+    static String user_id;
+    File user_file;
+    FileWriter filewriter;
+    
+    StoredUsers(){}
+    
+    StoredUsers(String user_id)
+        {
+            this.user_id = user_id;
+        }
+    /*
+    erstellt ein users.txt
+    */
+    public void createUsers()
     {
-        File file = new File("Group_id.txt");
-        StringBuilder fileContents = new StringBuilder((int)file.length());
-        Scanner scanner = new Scanner(file);
-        String lineSeperator = System.getProperty("MussNenStringEinfuegen");
-        String username = scanner.nextLine();
         try
         {
-            while(scanner.hasNextLine())
-            {
-                fileContents.append(scanner.nextLine()).append(lineSeperator);
-                String output = username.substring(0, username.indexOf(":"));
-                System.out.println(output);
-            }
-            //Als reines Beispiel
-            return fileContents.toString();
+            user_file = new File("users.txt");
+            filewriter = new FileWriter(user_file ,true);
+            filewriter.flush();
+            filewriter.close();
         }
-        
-        finally
-        {
-            scanner.close();
-        }        
+            catch (IOException e) 
+            {
+//                e.printStackTrace();
+            }  
     }
-    //Nur ein Test
+    /*
+    Nimmt nen user_id String und schreibt ihn, in die .txt Datei.
+    */
+    public String pushUsers(String user_id)
+    {   
+        try
+        {
+            user_file = new File("users.txt");
+            if(!user_file.exists())
+            {
+                System.out.println("Test, existiert nicht");
+            }
+            
+            else
+            {
+                filewriter = new FileWriter(user_file ,true);
+                filewriter.write("\n" + user_id);
+                filewriter.flush();
+                filewriter.close();
+            }
+        }
+        catch (IOException e) {
+//            e.printStackTrace();
+        }
+         return user_id;
+        
+    }
+    /*
+    Braucht man nicht unbedingt, ich lass es trotzdem drin
+    */
+    public void checkIfUserExists(String user_id) throws FileNotFoundException, IOException
+    {
+        BufferedReader br = new BufferedReader(new FileReader("users.txt"));
+        String line;
+        while ((line = br.readLine()) != null) 
+        {
+        if (line.contains(user_id))
+            {
+                System.out.println("Username existiert schon!");
+            }
+        else
+            {
+                System.out.println("User wurde gespeichert!");
+            }
+        }
+    }
+    
+    /*
+    löscht doppelte usernamen aus der Liste raus
+    */
+public void stripDuplicatesFromFile() throws FileNotFoundException, IOException 
+{
+    Set<String> lines;
+        try (BufferedReader reader = new BufferedReader(new FileReader("users.txt"))) {
+            lines = new HashSet<>(10000); // maybe should be bigger
+            String line;
+            while ((line = reader.readLine()) != null)
+            {
+                lines.add(line);
+            }   } // maybe should be bigger
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("users.txt"))) 
+        {
+            for (String unique : lines)
+            {
+                writer.write(unique);
+                writer.newLine();
+            }   
+        }
+}
+
+    //Nur zum testen
     public static void main(String[] args) throws FileNotFoundException, IOException
     {
         StoredUsers test = new StoredUsers();
-        test.readFile();
+        test.createUsers();
+        test.pushUsers("Max"); 
+        test.pushUsers("Maxi");  
+        test.pushUsers("Mexiko");
+        //test.checkIfUserExists("Max");
+        test.stripDuplicatesFromFile();
     }
 }
-
 
 
