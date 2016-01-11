11a63e638656829343a01d5a2dfe1bcca7013b49
Tobias <muellertobias789@gmail.com>
2016-01-11 14:57:11 +0100

    Klasse für das System mit den wichtigsten Konstanten hinzufügt (Port, Name der Services,...)

---
+++ IDL-Definitions/src/BoardConfiguration/BoardConfiguration.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BoardConfiguration;
+
+/**
+ *
+ * @author Tobias
+ */
+public class BoardConfiguration {
+    public static final String ORB_PORT = "1050";
+    
+    public static final String NAMESERVICE = "NameService";
+    
+    public static final String ADMIN_SERVICE_NAME = "AdminService";
+    public static final String BOARD_SERVICE_NAME = "BoardService";
+    public static final String VIEW_SERVICE_NAME = "ViewService";
+    public static final String VGROUP_SERVICE_NAME = "VirtualGroupService";
+}
