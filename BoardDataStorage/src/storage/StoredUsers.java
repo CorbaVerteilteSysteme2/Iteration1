9bf1dadda4eee433537b0fa135ec505190dc6fef
user <user@HP>
2016-01-12 23:01:24 +0100

    User aus verschiedenen "Tafeln" auslesen. Interface-Implementierung folgt noch asap. Code Aufräumen ebenso.

--- BoardDataStorage/src/storage/StoredUsers.java
+++ BoardDataStorage/src/storage/StoredUsers.java
@@ -1,5 +1,7 @@
 package storage;
 
+import BoardModules.User;
+import Interfaces.IUserStorage;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
@@ -11,6 +13,8 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -28,7 +32,7 @@ import java.util.Set;
  * Die Variable "user_id" u.U. ebenso.
  * 
  */
-public class StoredUsers {
+public class StoredUsers{
 
     static String user_id;
     File user_file;
@@ -42,12 +46,14 @@ public class StoredUsers {
         }
     /**
      * Erstellt eine "users.txt" Datei
+     * @param identifier
      */
-    public void createUsers()
+    /*
+    public void createUsers(String identifier)
     {
         try
         {
-            user_file = new File("users.txt");
+            user_file = new File(identifier + "users.txt");
             filewriter = new FileWriter(user_file ,true);
             filewriter.flush();
             filewriter.close();
@@ -57,38 +63,53 @@ public class StoredUsers {
 //                e.printStackTrace();
             }  
     }
+    */
     /**
-     * Bekommt einen String und schreibt diesen in die
+     * Schreibt die User in bestimmte Dateien, abhängig vom identifier
      * users.txt Datei
+     * @param identifier
      * @param user_id (Name der User)
      * @return user_id
      */
-    public String pushUsers(String user_id)
+    public User pushUsersToBoard(String identifier, String user_id)
     {   
+        User return_User = null;
         try
         {       
+                user_file = new File(identifier + "users.txt");
                 filewriter = new FileWriter(user_file ,true);
-                filewriter.write("\n" + user_id);
-                filewriter.flush();
-                filewriter.close();
+                if(user_file.exists())
+                {
+                                    
+                    filewriter.write("\n" + user_id);
+                    filewriter.flush();
+                    filewriter.close();
+                    
+                }
+                else
+                {
+                    filewriter.write("Kaputt");
+                }
+
         }
         catch (IOException e) {
 //            e.printStackTrace();
         }
-         return user_id;
+         return return_User;
         
     }
     /**
      * Prueft ob bestimmte UserNamen in users.txt bereits vorhanden sind.
      * War ursprünglich für was anderes gedacht, aber könnte trotzdem
      * nützlich sein, deshalb hab ich es drin gelassen.
+     * @param identifier
      * @param user_id
      * @throws FileNotFoundException
      * @throws IOException 
      */
-    public void checkIfUserExists(String user_id) throws FileNotFoundException, IOException
+    public void checkIfUserExists(String identifier, String user_id) throws FileNotFoundException, IOException
     {
-        BufferedReader br = new BufferedReader(new FileReader("users.txt"));
+        BufferedReader br = new BufferedReader(new FileReader(identifier + "users.txt"));
         String line;
         while ((line = br.readLine()) != null) 
         {
@@ -109,13 +130,14 @@ public class StoredUsers {
      * Ruft users.txt auf und prüft auf doppelte Nutzernamen.
      * Da man am Ende keine Datei haben will, in der 50 mal derselbe user
      * drin steht, werden doppelte Usernamen auch gleich gelöscht.
+     * @param identifier
      * @throws FileNotFoundException
      * @throws IOException 
      */
-public void stripDuplicatesFromFile() throws FileNotFoundException, IOException 
+public void stripDuplicatesFromFile(String identifier) throws FileNotFoundException, IOException 
 {
     Set<String> lines;
-        try (BufferedReader reader = new BufferedReader(new FileReader("users.txt")))
+        try (BufferedReader reader = new BufferedReader(new FileReader(identifier + "users.txt")))
         {
             lines = new HashSet<>(10000);
             String line;
@@ -124,7 +146,7 @@ public void stripDuplicatesFromFile() throws FileNotFoundException, IOException
                 lines.add(line);
             }   
         }
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter("users.txt"))) 
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(identifier + "users.txt"))) 
         {
             for (String unique : lines)
             {
@@ -138,12 +160,14 @@ public void stripDuplicatesFromFile() throws FileNotFoundException, IOException
  * Methode liest die einzelnen User aus der .txt Datei in ein Array.
  * Die erste Zeile ist ein newline, Array-Index fängt also bei 1 an.
  * 
+     * @param identifier
  * @throws FileNotFoundException
  * @throws IOException 
  */
-    public void readUsersFromTxt() throws FileNotFoundException, IOException
+/*
+    public void readUsersFromTxt(String identifier) throws FileNotFoundException, IOException
     {
-        BufferedReader in = new BufferedReader(new FileReader("users.txt"));
+        BufferedReader in = new BufferedReader(new FileReader(identifier + "users.txt"));
         String user;
         
         List<String> list = new ArrayList<>();
@@ -152,8 +176,49 @@ public void stripDuplicatesFromFile() throws FileNotFoundException, IOException
             list.add(user);
         }
         
-        String[] userNameArray = list.toArray(new String[0]);
+        String[] userNameArray = list.toArray(new String[2]); 
+        System.out.println(userNameArray);
+    }
+    */
+
+    public ArrayList<User> ReadUsersFromTxtFile(String identifier) throws IOException
+    {
+        User single_user;
+        ArrayList<User> user_IDs = new ArrayList<>();
+        String zeile;
         
+        try
+        {
+            user_file = new File(identifier + "users.txt");
+            if(!user_file.exists())
+            {
+                System.out.println("Die Tafel exisitiert nicht!");
+            }
+            else
+            {
+                FileReader filereader = new FileReader(identifier + "users.txt");
+                BufferedReader bufferedreader = new BufferedReader(filereader);
+                while((zeile=bufferedreader.readLine()) != null)
+                {
+                    user_id = "";
+                    
+                    for (int i=1; i < zeile.length(); i++)
+                    {
+                        user_id += zeile.charAt(i);
+                    }
+                    single_user = new User(user_id);
+                    user_IDs.add(single_user);
+                }
+                bufferedreader.close();
+            }
+
+        }
+        catch (IOException e)
+        {
+            //e.printStackTrace();
+        }
+        
+        return user_IDs;
     }
 }
 
